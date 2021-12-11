c083d749a2463b7b7979057cb681c4b6f9b9763e
mking93 <18111993tmvmking>
2021-12-11 20:50:33 +0300

    HW2 unpack string completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,55 @@
-package hw02unpackstring
+package main
 
 import (
-	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+func Unpack(str string) (string, error) {
+	result := ""
+	strBefore := ""
+	for i := 0; i < len(str); i++ {
+		if (i == 0 || (i+1 < len(str) && isInt(string(str[i+1])))) && isInt(string(str[i])) {
+			result = ""
+			break
+		} else if isInt(string(str[i])) {
+			v, _ := strconv.Atoi(string(str[i]))
+			if v > 0 {
+				if string(str[i-1]) == "\n" {
+					result = result[:len(result)-1]
+					result += strings.Repeat("\\n", v)
+				} else {
+					result += strings.Repeat(strBefore, v-1)
+				}
+			} else {
+				result = result[:len(result)-1]
+			}
+		} else {
+			strBefore = string(str[i])
+			result += strBefore
+		}
+	}
+	return result, nil
+}
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func main() {
+	fmt.Println(Unpack("a4bc2d5e"))
+	fmt.Println(Unpack("abcd"))
+	fmt.Println(Unpack("3abc"))
+	fmt.Println(Unpack("45"))
+	fmt.Println(Unpack("aaa10b"))
+	fmt.Println(Unpack("aaa0b"))
+	fmt.Println(Unpack(""))
+	fmt.Println(Unpack("d\n5abc"))
+}
+
+func isInt(s string) bool {
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
 }
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,26 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
+	ch := make(chan Task)
+	wg := sync.WaitGroup{}
+
+	var errCnt int32
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for task := range ch {
+				if err := task(); err != nil {
+					atomic.AddInt32(&errCnt, 1)
+				}
+			}
+		}()
+	}
+
+	for _, task := range tasks {
+
+	}
 	// Place your code here.
 	return nil
 }
